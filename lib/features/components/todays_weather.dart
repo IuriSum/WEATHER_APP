9c3f2cc12b2001450425e8779666c6e3c8aa5dd6
unknown <iuri.sumikawa@gmail.com>
2024-10-21 21:43:59 -0300

    banner principal de clima esta feito

--- lib/features/components/todays_weather.dart
+++ lib/features/components/todays_weather.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:weather_forecast/core/models/day.dart';
 
-class TodaysWeatherBanner extends StatelessWidget {
-
-  final DayModel dayModel;
-
-  const TodaysWeatherBanner({
-    super.key,
-    required this.dayModel
-  });
+class _TodaysWeatherBannerState extends State<TodaysWeatherBanner> {
+  bool pressed = false;
 
   String _todaysText(){
     DateTime now = DateTime.now();
@@ -17,103 +11,179 @@ class TodaysWeatherBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      decoration: BoxDecoration(
-        color: Colors.lightBlue,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.white, width: 1)
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(171, 44, 41, 41),
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.white, width: 1)
+    return InkWell(
+      onTap: () => setState(() {
+        pressed = !pressed;
+      }),
+      child: Container(
+        margin: EdgeInsets.all(8),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.lightBlue,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.white, width: 1)
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(171, 44, 41, 41),
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.white, width: 1)
+              ),
+              child: Text(
+                widget.dayModel.city,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
             ),
-            child: Text(
-              dayModel.city,
+            SizedBox(height: 12,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(171, 44, 41, 41),
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.white, width: 1)
+              ),
+              child: Text(
+                _todaysText(),
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Text(
+              "${widget.dayModel.currentTemp}°",
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 80,
                 color: Colors.white,
               ),
             ),
-          ),
-          SizedBox(height: 12,),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(171, 44, 41, 41),
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.white, width: 1)
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  widget.dayModel.currentStatus,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "${widget.dayModel.maxTemp}°",
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    height: 0
+                  ),
+                ),
+                 Text(
+                  "/",
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    height: 0
+                  ),
+                ),
+                Text(
+                  "${widget.dayModel.minTemp}°",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    height: 0
+                  ),
+                ),
+                AnimatedRotation(
+                  turns: pressed? 0.5 : 0.0, 
+                  duration: Duration(milliseconds: 200),
+                  child: Icon(
+                    Icons.keyboard_arrow_down,
+                    size: 26,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-            child: Text(
-              _todaysText(),
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "${dayModel.currentTemp}°",
-                style: TextStyle(
-                  fontSize: 80,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                dayModel.currentStatus,
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Spacer(),
-              Text(
-                "${dayModel.maxTemp}°",
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.white,
-                  height: 0
-                ),
-              ),
-               Text(
-                "/",
-                style: TextStyle(
-                  fontSize: 32,
-                  color: Colors.white,
-                  height: 0
-                ),
-              ),
-              Text(
-                "${dayModel.minTemp}°",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  height: 0
-                ),
+            AnimatedSize(
+                duration: Duration(milliseconds: 200),
+                child: pressed?
+                  Container(
+                    margin: EdgeInsets.only(top: 16),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.water_drop,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "${widget.dayModel.precipitation} mm/hr",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            height: 0
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.air,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "${(widget.dayModel.windSpeed*3.6).toStringAsFixed(2)} km/h",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            height: 0
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.sunny,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          " UV ${widget.dayModel.uv} (0-11)",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            height: 0
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                :
+                  Container()
               )
-            ],
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
+}
+
+class TodaysWeatherBanner extends StatefulWidget {
+
+  final DayModel dayModel;
+
+  const TodaysWeatherBanner({
+    super.key,
+    required this.dayModel
+  });
+
+  @override
+  State<TodaysWeatherBanner> createState() => _TodaysWeatherBannerState();
 }
