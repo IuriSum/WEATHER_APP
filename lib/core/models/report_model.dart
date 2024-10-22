953019963c308fa4507c2131949be82099da8b4a
unknown <iuri.sumikawa@gmail.com>
2024-10-22 09:38:52 -0300

    correção dos models e diretórios

---
+++ lib/core/models/report_model.dart
@@ -0,0 +1,16 @@
+
+import 'package:hive/hive.dart';
+
+part 'report_model.g.dart';
+
+@HiveType(typeId: 1) //typeId should be unique for each model
+class ReportModel {
+  @HiveField(0) //uinque id for each field
+  late String name;
+
+  @HiveField(1)
+  late DateTime date;
+
+  @HiveField(2)
+  late String report;
+}
---
+++ lib/core/services/report_service.dart
@@ -0,0 +1,33 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_forecast/core/models/report_model.dart';
+
+class Service {
+final String  _boxName = "reportBox";
+
+Future<Box<ReportModel>> get _box async =>
+      await Hive.openBox<ReportModel>(_boxName);
+
+//create
+  Future<void> addReport(ReportModel reportModel) async {
+    var box = await _box;
+    await box.add(reportModel);
+  }
+
+//read
+  Future<List<ReportModel>> getAllReports() async {
+    var box = await _box;
+    return box.values.toList();
+  }
+
+//update
+  Future<void> updateDeck(int index, ReportModel report) async { 
+    var box = await _box;
+    await box.putAt(index, report);
+  }
+
+//delete
+  Future<void> deleteReport(int index) async {
+    var box = await _box;
+    await box.deleteAt(index);
+  }
+}
