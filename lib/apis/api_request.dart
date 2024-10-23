9bda29763ae82a2d6e3708aa78251abb4430d75b
unknown <iuri.sumikawa@gmail.com>
2024-10-22 23:49:06 -0300

    agrupamentos de infos de request por dia

--- lib/apis/api_request.dart
+++ lib/apis/api_request.dart
@@ -1,11 +1,14 @@
 
 
 import 'dart:convert';
-// import 'package:dio/dio.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:weather_forecast/models/day_model.dart';
 
 class ApiHandler {
+
+  // Putting API keys like this is wrong, but for quick testing, I'll leave it.
+
   static const String HOURLY_URL = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/hourly';
   
   static final Map<String,dynamic> _headers = {
@@ -15,26 +18,26 @@ class ApiHandler {
 
   static Future<Map<String, dynamic>?> weatherForecast120() async {
     try{
-      // final Map<String,dynamic> _params = {
-      //   "lat" : -25.4284,
-      //   "lon" : -49.2733,
-      //   "hours" : "120"
-      // };
+      final Map<String,dynamic> _params = {
+        "lat" : -25.4284,
+        "lon" : -49.2733,
+        "hours" : "120"
+      };
 
-      // Response response = await Dio().get(
-      //   HOURLY_URL,
-      //   queryParameters: _params,
-      //   options: Options(
-      //     sendTimeout: Duration(seconds: 30),
-      //     receiveTimeout: Duration(seconds: 30),
-      //     headers: _headers,
-      //     responseType: ResponseType.json
-      //   ),
-      // );
+      Response response = await Dio().get(
+        HOURLY_URL,
+        queryParameters: _params,
+        options: Options(
+          sendTimeout: Duration(seconds: 30),
+          receiveTimeout: Duration(seconds: 30),
+          headers: _headers,
+          responseType: ResponseType.json
+        ),
+      );
 
-      // return response.data;
-      String asset = await rootBundle.loadString('assets/mocked_response.json');
-      return jsonDecode(asset);
+      return response.data;
+      // String asset = await rootBundle.loadString('assets/mocked_response.json');
+      // return jsonDecode(asset);
     }
     catch(e){
       return null;
@@ -46,13 +49,29 @@ class ApiHandler {
     try{
       List <DayModel> days = [];
       Map<String, dynamic>? requestInfo = await weatherForecast120();
-        List<dynamic> data = requestInfo!["data"]??[];
-        // divides request in 3 days
-        for (int i = 0; i < 71; i += 24) {
-          List<dynamic> sublist = data.sublist(i, i + 24);
-          days.add(DayModel(weatherInfo: sublist, city: requestInfo["city_name"]));
+
+      // Group itens by date
+      Map<String, List<Map<String, dynamic>>> groupedByDate = {};
+
+      for (var item in requestInfo!["data"]) {
+        String date = item['timestamp_local'].split('T')[0];
+        if (groupedByDate.containsKey(date)) {
+          groupedByDate[date]!.add(item);
+        } else {
+          groupedByDate[date] = [item];
         }
-        return days;
+      }
+
+      // grouping the first 3 days
+      List<String> keys = groupedByDate.keys.take(3).toList();
+      List<dynamic> result = keys.map((key) => groupedByDate[key]).toList();
+      
+      // generating our Days List
+      for(int i =0; i<result.length; i++){
+        days.add(DayModel(weatherInfo: result[i], city: requestInfo["city_name"]));
+      }
+
+      return days;
     }
     catch(e){
       return null;
